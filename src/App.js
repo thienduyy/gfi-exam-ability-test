77f748a831205898e7ae446646cf32e0e9f2c8ce
Thien Duy <nguyenvthienduy@gmail.com>
2023-06-08 16:30:31 +0700

    Fix - Remove path

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import './App.scss';
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from './components/HomePage';
 import AppComponent from './components/App';
-import Profile from './components/Profile';
-import Matched from './components/Matched';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
@@ -20,9 +18,6 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/app" element={<RequireAuth><AppComponent /></RequireAuth>} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/matched" element={<Matched />} />
-          {/* <Header /> */}
         </Routes>
       </Router>
     </div>
